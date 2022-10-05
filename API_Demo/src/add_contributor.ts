c02720137c2d35951a5d0c8792bd7859939bc434
bodrovis <bodrovis@protonmail.com>
2022-10-05 19:44:23 +0300

    NodeSDK v9

--- API_Demo/src/add_contributor.ts
+++ API_Demo/src/add_contributor.ts
@@ -1,7 +1,9 @@
-import { LokaliseApi } from '@lokalise/node-api';
+import { LokaliseApi } from "@lokalise/node-api";
 
 async function main() {
-
+  const lokaliseApi = new LokaliseApi({ apiKey: ''});
+  const projects = await lokaliseApi.projects().list();
+  console.log(projects.items[0].name);
 }
 
 main()
